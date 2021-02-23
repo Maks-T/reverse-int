384c97ba83887779010e911be41a5661ddc7bd2e
Maxim Tsatsura <maxim.tsatsura@gmail.com>
2021-02-23 21:17:45 +0300

    feat: add subtask reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n == undefined) return 0;
+    if (isNaN(n)) return 0;  
+   
+    if (n<0) { 
+        n = -1*n; 
+        return Number(n.toString().split('').reverse().join(''));
+          
+    } else {
+        return  Number(n.toString().split('').reverse().join(''));
+          
+    }
+ 
+   
+ }
